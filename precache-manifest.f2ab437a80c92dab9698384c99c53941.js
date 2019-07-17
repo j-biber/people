f6e0a8b0583c1b892c00aeccea5e41bcbbc55102
j-biber <jelena.biberovic@gmail.com>
2019-07-17 15:55:47 +0200

    Updates

---
+++ precache-manifest.f2ab437a80c92dab9698384c99c53941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048142abb04e901c2f3a144cb3f788a8",
+    "url": "/people/index.html"
+  },
+  {
+    "revision": "82b3d90bd14259076438",
+    "url": "/people/static/css/main.9590e744.chunk.css"
+  },
+  {
+    "revision": "08ca966498d955f1c83b",
+    "url": "/people/static/js/2.3d8dcad6.chunk.js"
+  },
+  {
+    "revision": "82b3d90bd14259076438",
+    "url": "/people/static/js/main.52b39276.chunk.js"
+  },
+  {
+    "revision": "cbc112f07b4df4486d3b",
+    "url": "/people/static/js/runtime~main.26c304f1.js"
+  }
+]);
